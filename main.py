07dec825290c92810bc7ce9dc46d06a9ac62bf2b
Sampan Naik <61792794+naiksampan@users.noreply.github.com>
2022-09-09 22:05:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
 import xgboost as xgb
 import numpy as np
