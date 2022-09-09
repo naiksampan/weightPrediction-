786b54a54410ce9baf08a86d367d595002aa2150
Sampan Naik <61792794+naiksampan@users.noreply.github.com>
2022-09-09 21:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 import numpy as np
 st.header("Human Weight Prediction App")
 st.text_input("Enter your Name: ", key="name")
-data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/weight-height.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/naiksampan/weightPrediction-/main/weight-height.csv")
 #load label encoder
 encoder = LabelEncoder()
 encoder.classes_ = np.load('classes.npy',allow_pickle=True)
